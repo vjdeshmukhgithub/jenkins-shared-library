7ddc1402c7d6f9ef734adfd859fdda6083cf1916
Xavier Coleman <coleman_xavier@bah.com>
2020-03-10 12:37:25 -0400

    removed secrets

--- resources/orchauth.ps1
+++ resources/orchauth.ps1
@@ -1,10 +1,12 @@
-$auth = @{
-   tenancyName = 'VerticalApps'
-   usernameOrEmailAddress = 'admin'
-   password = 'myOrchestratorVapps11'
+function orchAuth($username, $pass){
+   $auth = @{
+      tenancyName = 'VerticalApps'
+      usernameOrEmailAddress = '$username'
+      password = '$pass'
+   }
+   $authjson = $auth | ConvertTo-Json
+   $authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
+   $authjson = $authkey | ConvertTo-Json
+   $token = $authjson | ConvertFrom-Json -AsHashtable
+   return $token.result
 }
-$authjson = $auth | ConvertTo-Json
-$authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
-$authjson = $authkey | ConvertTo-Json
-$token = $authjson | ConvertFrom-Json -AsHashtable
-return $token.result
