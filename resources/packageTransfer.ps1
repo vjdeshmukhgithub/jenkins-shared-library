4939487084d097094a5dfc70c607aef916e66a38
Xavier Coleman <coleman_xavier@bah.com>
2020-03-10 12:24:53 -0400

    add auth forn package transfer script and readme

--- resources/packageTransfer.ps1
+++ resources/packageTransfer.ps1
@@ -1,1 +1,12 @@
+$auth = @{
+    tenancyName = 'VerticalApps'
+    usernameOrEmailAddress = 'admin'
+    password = 'myOrchestratorVapps11'
+ }
+ $authjson = $auth | ConvertTo-Json
+ $authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
+ $authjson = $authkey | ConvertTo-Json
+ $token = $authjson | ConvertFrom-Json -AsHashtable
+ return $token.result
+
 Write-Host "If the scan result from sonarqube passes the quality gate move the package. If not fail the build"
