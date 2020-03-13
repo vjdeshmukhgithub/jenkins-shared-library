080739f69526af8aa0016f0facb69de3597d5e68
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-13 09:41:41 -0400

    Added comments to library

--- vars/sonarQubeScan.groovy
+++ vars/sonarQubeScan.groovy
@@ -1,7 +1,9 @@
 def call() {
     script {
+        // Must match name of SonarQube Scanner installation on Jenkins server
         scannerHome = tool 'SonarScanner'
     }
+    // Must match the name of SonarQube server
     withSonarQubeEnv('Vertical Apps SonarQube') {
         bat "\"${scannerHome}\\bin\\sonar-scanner.bat\" -D sonar.projectKey=${env.JOB_NAME}"
     }
