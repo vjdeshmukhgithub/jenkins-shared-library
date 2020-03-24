910e7aa9453083cdef2ef8ee49ab12d23b4d2117
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-24 16:43:46 -0400

    Maybe the triple quotes?

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,2 @@
 $ErrorActionPreference='Stop'
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "\"$env:WORKSPACE\"" --output "\"$env:WORKSPACE@tmp\Output\""
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack """$env:WORKSPACE""" -o """$env:WORKSPACE@tmp\Output\"""
