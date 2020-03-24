870fab97d28a015c1bb76f25a836fbf8574fdaa0
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-24 16:27:45 -0400

    Correcting issue around workspace

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,1 +1,1 @@
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\$env:JOB_NAME" --output "$env:WORKSPACE\$env:JOB_NAME@tmp\Output"
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE" --output "$env:WORKSPACE@tmp\Output"
