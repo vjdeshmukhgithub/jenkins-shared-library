f970bc21ba163f4c0b148d724f346bdd628dfee1
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-24 16:36:44 -0400

    Taking a shot with adding quotes

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,1 +1,2 @@
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE" --output "$env:WORKSPACE@tmp\Output"
+$ErrorActionPreference='Stop'
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "\"$env:WORKSPACE\"" --output "\"$env:WORKSPACE@tmp\Output\""
