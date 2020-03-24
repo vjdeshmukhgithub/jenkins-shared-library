8e28ad34cb8774218b3e076be84d19211663ac62
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-24 16:52:35 -0400

    This seems unlikely to work

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,2 @@
-Write-Output "$env:WORKSPACE"
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack ""$env:WORKSPACE"" -o ""$env:WORKSPACE@tmp\Output\""
+Write-Output $env:WORKSPACE
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack $env:WORKSPACE -o $env:WORKSPACE@tmp\Output\
