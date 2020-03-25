551ed82e69f74c888a9c910f7ce93ba40a4e6aaf
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 19:09:26 -0400

    retrying powershell

--- vars/pack.groovy
+++ vars/pack.groovy
@@ -1,5 +1,4 @@
 def call() {
-    //def psscript = libraryResource 'pack.ps1'
-    //psCall = powershell script: psscript
-    sh(script:"\"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe\" pack \"$env:WORKSPACE\project.json\" -o \"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER\"")
+    def psscript = libraryResource 'pack.ps1'
+    psCall = powershell script: psscript
 }
