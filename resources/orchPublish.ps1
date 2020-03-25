e5b6aa5d0208ba10de86d40bd40e674d9a000c6e
Xavier Coleman <coleman_xavier@bah.com>
2020-03-25 15:13:59 -0400

    Update orchPublish.ps1

--- resources/orchPublish.ps1
+++ resources/orchPublish.ps1
@@ -7,6 +7,6 @@ $authjson = $auth | ConvertTo-Json
 $authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
 $authjson = $authkey | ConvertTo-Json
 $token = $authjson | ConvertFrom-Json -AsHashtable
-
+Write-Output $token
 Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage' -Method Post -Authentication Bearer -Token $token.result -InFile 'C:\\Program Files (x86)\\Jenkins\\workspace\\test-shared-library@tmp\\Output\\Test.1.0.7.nupkg'
 # //curl -XPOST --insecure -v -H 'Authorization: Bearer $token' '-F file=@C:\\Program Files (x86)\\Jenkins\\workspace\\test-shared-library@tmp\\Output\\Test.1.0.7.nupkg' 'https://uipath.verticalapps.com/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage'
