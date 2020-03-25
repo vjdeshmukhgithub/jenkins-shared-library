756bff4094a8b25d0d0d2476fe763a96d35fc6e6
Xavier Coleman <coleman_xavier@bah.com>
2020-03-25 16:54:12 -0400

    Update orchPublish.groovy

--- vars/orchPublish.groovy
+++ vars/orchPublish.groovy
@@ -2,4 +2,6 @@ def call() {
     def psscript = libraryResource 'orchPublish.ps1'
 
     psCall = powershell returnStatus: true, script: psscript
+
+    if(status != 0) { powershell ("Write-Error \"Ooops, an error occured\"") }
 }
