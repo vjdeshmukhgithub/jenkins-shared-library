340e5072beadd8fdf92007aaa9d323687faba352
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 14:53:34 -0400

    Changing output folder

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,1 +1,1 @@
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:WORKSPACE@tmp"
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
