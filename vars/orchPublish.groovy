acc46615fc0caa1392578df062064208c0e21ec9
Xavier Coleman <coleman_xavier@bah.com>
2020-03-25 14:01:51 -0400

    Update orchPublish.groovy

--- vars/orchPublish.groovy
+++ vars/orchPublish.groovy
@@ -1,14 +1,29 @@
+import groovy.json.JsonSlurper
+
+String jsonString = '''{
+"result": "",
+"targetUrl": "",
+"success": "",
+"error": "",
+"unAuthorizedRequest": "",
+"__abp": ""
+}'''
+
+JsonSlurper slurper = new JsonSlurper()
+
+Map parsedJson = slurper.parseText(jsonString)
+
 def call() {
-    $auth = @{
-      tenancyName = 'VerticalApps'
-      usernameOrEmailAddress = 'admin'
-      password = 'myOrchestratorVapps11'
-   }
-   $authjson = $auth | ConvertTo-Json
-   $authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
-   $authjson = $authkey | ConvertTo-Json
-   $token = $authjson | ConvertFrom-Json -AsHashtable
+   Map parsedJson = slurper.parseText(curl --location --insecure --request POST 'https://uipath.verticalapps.com/api/Account/Authenticate' \
+      --header 'Content-Type: application/json' \
+      --data-raw '{
+      "tenancyName": "VerticalApps",
+      "usernameOrEmailAddress": "admin",
+      "password": "myOrchestratorVapps11"
+      }')
    
-   curl -XPOST --insecure -H 'Authorization: Bearer $token' '-F file=@C:\\Program Files (x86)\\Jenkins\\workspace\\test-shared-library@tmp\\Output\\Test.1.0.7.nupkg' 'https://uipath.verticalapps.com/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage'
+   token = parsedJson.result
+
+   curl -XPOST --insecure -v -H 'Authorization: Bearer $token' '-F file=@C:\\Program Files (x86)\\Jenkins\\workspace\\test-shared-library@tmp\\Output\\Test.1.0.7.nupkg' 'https://uipath.verticalapps.com/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage'
 
 }
