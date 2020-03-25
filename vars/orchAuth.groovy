056555a3cbde44cb598eb5cdb96228b21d692c99
Xavier Coleman <coleman_xavier@bah.com>
2020-03-25 15:12:12 -0400

    Update orchAuth.groovy

--- vars/orchAuth.groovy
+++ vars/orchAuth.groovy
@@ -1,5 +1,5 @@
 def call(String ORCH_USER, String ORCH_PASS) {
     def psscript = libraryResource 'orchAuth.ps1'
 
-    psCall = powershell returnStatus: true, script: psscript()
+    psCall = powershell returnStatus: true, script: psscript
 }
