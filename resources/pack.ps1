f26190246e15163a465f7c4ece32c58513355796
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 19:21:24 -0400

    remove print

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,1 @@
-Write-Output "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
 & "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
