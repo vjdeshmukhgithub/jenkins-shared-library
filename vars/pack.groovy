979efb45d520fda01fe370b41214ef29f14055cb
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 15:44:26 -0400

    trying with cmd

--- vars/pack.groovy
+++ vars/pack.groovy
@@ -1,4 +1,5 @@
 def call() {
-    def psscript = libraryResource 'pack.ps1'
-    psCall = powershell script: psscript
+    //def psscript = libraryResource 'pack.ps1'
+    //psCall = powershell script: psscript
+    sh(script:"\"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe\" pack \"$env:WORKSPACE\project.json\" -o \"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER\"")
 }
