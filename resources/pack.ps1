4fa767715dddd33b9d5496e223c1abbe4f81d509
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 21:27:54 -0400

    Apparently we'll just trust anything now

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,7 +1,1 @@
-#"& 'C:\Program Files (x86)\UiPath\Studio\UiRobot.exe' pack '$env:WORKSPACE\project.json' -o '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'";
-$pwshScriptBlock = @'
-& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"" *> script.log
-'@;
-$FilePath = "C:\Program Files\PowerShell\7\pwsh.exe"
-$ArgumentList ='-noprofile -nologo - NonInteractive -w hidden -command "& {' + $pwshScriptBlock + '}"';
-$Process = Start-Process -FilePath $FilePath -ArgumentList $ArgumentList -PassThru;
+.\'C:\Program Files (x86)\UiPath\Studio\UiRobot.exe' pack '$env:WORKSPACE\project.json' -o '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'
