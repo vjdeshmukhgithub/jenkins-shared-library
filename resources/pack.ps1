216eed1d70c52def28f575d9a7b4b663bc53662b
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 22:56:27 -0400

    re-add pack

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,10 +1,11 @@
-cd 'C:\Program Files (x86)\UiPath\Studio'
+"& 'C:\Program Files (x86)\UiPath\StudioUiRobot.exe' pack '$env:WORKSPACE\project.json' --output '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'"
+#cd 'C:\Program Files (x86)\UiPath\Studio'
 #START UiRobot.exe -ArgumentList @("pack", "$env:WORKSPACE\project.json", "--output", "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER") 
-$wkspace = $env:WORKSPACE
-$jenkhome = $env:JENKINS_HOME
-$jbname = $env:JOB_NAME
-$bldnum = $env:BUILD_NUMBER
-$out = "$jenkhome\jobs\$jbname\builds\$bldnum"
-$proj = "$wkspace\project.json"
+#$wkspace = $env:WORKSPACE
+#$jenkhome = $env:JENKINS_HOME
+#$jbname = $env:JOB_NAME
+#$bldnum = $env:BUILD_NUMBER
+#$out = "$jenkhome\jobs\$jbname\builds\$bldnum"
+#$proj = "$wkspace\project.json"
 
-START UiRobot.exe -ArgumentList @("pack", $proj, "--output", $out)
+#START UiRobot.exe -ArgumentList @("pack", $proj, "--output", $out)
