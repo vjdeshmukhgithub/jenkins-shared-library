65d084162b7a50d3615962b0415f7fcb0f6092bc
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 20:54:19 -0400

    all the logs

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,1 @@
-"& \`"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe\`" pack \`"$env:WORKSPACE\project.json\`" -o \`"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER\`"" 2> error.txt
-Get-Content -Path error.txt
+"& \`"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe\`" pack \`"$env:WORKSPACE\project.json\`" -o \`"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER\`"" *> script.log
