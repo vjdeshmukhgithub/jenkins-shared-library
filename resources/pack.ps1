2070d8c716e43c753ece9cef3033f62e48d915a5
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 21:42:12 -0400

    Starter jackets

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,2 @@
 cd 'C:\Program Files (x86)\UiPath\Studio'
-.\UiRobot pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
+START UiRobot.exe pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
