22885dc7857d1807cf5cc355305506ac5d590f8a
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 20:57:36 -0400

    sure, why not?

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,1 +1,1 @@
-"& \`"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe\`" pack \`"$env:WORKSPACE\project.json\`" -o \`"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER\`"" *> script.log
+"& `"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe`" pack `"$env:WORKSPACE\project.json`" -o `"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER`"" *> script.log
