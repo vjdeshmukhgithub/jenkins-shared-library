fd2fc71e797d4a9c363b5760e07e74fa7f1520e5
Xavier Coleman <coleman_xavier@bah.com>
2020-03-26 08:55:35 -0400

    demo spiffy

--- resources/orchPublish.ps1
+++ resources/orchPublish.ps1
@@ -3,13 +3,17 @@ $auth = @{
    usernameOrEmailAddress = 'admin'
    password = 'myOrchestratorVapps11'
 }
+Write-Output "Beginning UIPath Orchestrator Authentication"
 $authjson = $auth | ConvertTo-Json
 $authkey = Invoke-RestMethod -SkipCertificateCheck 'https://uipath.verticalapps.com/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
 $authjson = $authkey | ConvertTo-Json
 $token = $authjson | ConvertFrom-Json
 Set-Variable -Name "ts" -Value $token.result
-Write-Output $ts
+Write-Output "Authenticated and token recieved"
+
 $tokenstring = ConvertTo-SecureString $ts -AsPlainText -Force
+
+Write-Output "Beginning UIPath Orchestrator publish"
 
 $FilePath = 'C:\Program Files (x86)\Jenkins\jobs\jlea-pipeline-test\builds\26\Test.1.0.8.nupkg'
 $FieldName = 'Test.1.0.8'
@@ -27,3 +31,4 @@ $MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
 $MultipartContent.Add($FileContent)
 
 Invoke-RestMethod -SkipCertificateCheck -Body $MultipartContent 'https://uipath.verticalapps.com/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage' -Method Post -Authentication Bearer -Token ($tokenstring)
+Write-Output "The package has been successfully published to Orchestrator and nexus"
