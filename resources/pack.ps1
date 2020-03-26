ba0e2407af346f9aa2bcd6a06f83982e057c9657
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 21:51:50 -0400

    different, but same

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,7 @@
 cd 'C:\Program Files (x86)\UiPath\Studio'
-START UiRobot.exe -ArgumentList @("pack", "$env:WORKSPACE\project.json", "--output", "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER") 
+#START UiRobot.exe -ArgumentList @("pack", "$env:WORKSPACE\project.json", "--output", "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER") 
+$wkspace = $env:WORKSPACE
+$jenkhome = $env:JENKINS_HOME
+$jbname = $env:JOB_NAME
+$bldnum = $env:BUILD_NUMBER
+START UiRobot.exe pack "$wkspace\project.json" --output "$jenkhome\jobs\$jbname\builds\$bldnum"
