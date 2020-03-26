d2e9afdbd8b5ab2e7fb76efe7b75addd4cbffd86
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 21:19:23 -0400

    yeah, still doin this

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,6 +1,6 @@
 #"& 'C:\Program Files (x86)\UiPath\Studio\UiRobot.exe' pack '$env:WORKSPACE\project.json' -o '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'";
 $pwshScriptBlock = @'
-"& `"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe`" pack `"$env:WORKSPACE\project.json`" -o `"$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER`"" *> script.log
+& "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"" *> script.log
 '@;
 $FilePath = "C:\Program Files\PowerShell\7\pwsh.exe"
 $ArgumentList ='-noprofile -nologo - NonInteractive -w hidden -command "& {' + $pwshScriptBlock + '}"';
