0d5cbbc8fb32164e93663af92dd57cac64bbaac5
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-26 14:51:31 -0400

    a dash of -

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,4 +1,4 @@
-"& 'C:\Program Files (x86)\UiPath\StudioUiRobot.exe' pack '$env:WORKSPACE\project.json' --output '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'"
+"& 'C:\Program Files (x86)\UiPath\StudioUiRobot.exe' -pack '$env:WORKSPACE\project.json' --output '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'"
 #cd 'C:\Program Files (x86)\UiPath\Studio'
 #START UiRobot.exe -ArgumentList @("pack", "$env:WORKSPACE\project.json", "--output", "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER") 
 #$wkspace = $env:WORKSPACE
