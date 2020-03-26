e34170e4f31a94f3d5ab87c4188aafb5aec870c1
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 21:34:38 -0400

    nothing else makes any sense

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,2 +1,2 @@
 cd 'C:\Program Files (x86)\UiPath\Studio'
-.\UiRobot.exe pack '$env:WORKSPACE\project.json' -o '$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER'
+.\UiRobot.exe pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
