ea29bcc6da61b1fb8fb1ee4a0c2247f1d952d2c7
Xavier Coleman <coleman_xavier@bah.com>
2020-03-25 20:01:06 -0400

    add call

--- vars/orchPublish.groovy
+++ vars/orchPublish.groovy
@@ -2,6 +2,5 @@ def call() {
     def psscript = libraryResource 'orchPublish.ps1'
 
     psCall = pwsh returnStatus: true, script: psscript
-
-    if(psCall != 0) { echo("Write-Error \"Ooops, an error occured\"") }
+    println psCall
 }
