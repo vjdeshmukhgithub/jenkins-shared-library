e096401295dbcba583e2816715b087d5f37ec0f1
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-03-25 20:11:08 -0400

    who knows vol 2?

--- resources/pack.ps1
+++ resources/pack.ps1
@@ -1,1 +1,1 @@
-& -VERBOSE "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
+-VERBOSE & "C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" pack "$env:WORKSPACE\project.json" -o "$env:JENKINS_HOME\jobs\$env:JOB_NAME\builds\$env:BUILD_NUMBER"
