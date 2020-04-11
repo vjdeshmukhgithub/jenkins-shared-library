85e43aa6a53e0196623e1e825ced5b6282130f49
jlea <jlea@CORP.AMAZONWORKSPACES.COM>
2020-04-11 14:11:31 -0400

    Working towards publish with parameters being passed to powershell

--- resources/orchPublish.ps1
+++ resources/orchPublish.ps1
@@ -1,22 +1,25 @@
 $auth = @{
-   tenancyName = ''
-   usernameOrEmailAddress = ''
-   password = ''
+   tenancyName = $args[0]
+   usernameOrEmailAddress = $args[1]
+   password = $args[2]
 }
+
 Write-Output "Beginning UIPath Orchestrator Authentication"
 $authjson = $auth | ConvertTo-Json
-$authkey = Invoke-RestMethod -SkipCertificateCheck 'https://path/api/Account/Authenticate' -Method Post -Body $authjson -ContentType 'application/json'
+$authkey = Invoke-RestMethod -SkipCertificateCheck "$($args[3])/api/Account/Authenticate" -Method Post -Body $authjson -ContentType 'application/json'
 $authjson = $authkey | ConvertTo-Json
 $token = $authjson | ConvertFrom-Json
 Set-Variable -Name "ts" -Value $token.result
-Write-Output "Authenticated and token recieved"
+Write-Output "$ts"
 
 $tokenstring = ConvertTo-SecureString $ts -AsPlainText -Force
 
 Write-Output "Beginning UIPath Orchestrator publish"
-
-$FilePath = 'C:\path\Test.1.0.10.nupkg'
-$FieldName = 'Test.1.0.10'
+ 
+$Directory = $args[4] 
+$Package = Get-ChildItem -Path $Directory -Filter *.nupkg
+$FilePath = $Directory + $Package.Name
+$FieldName = $Package.Name.Replace(".nupkg","")
 $ContentType = 'multipart/form-data'
 
 $FileStream = [System.IO.FileStream]::new($filePath, [System.IO.FileMode]::Open)
@@ -30,5 +33,5 @@ $FileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue
 $MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
 $MultipartContent.Add($FileContent)
 
-Invoke-RestMethod -SkipCertificateCheck -Body $MultipartContent 'https://path/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage' -Method Post -Authentication Bearer -Token ($tokenstring)
+Invoke-RestMethod -SkipCertificateCheck -Body $MultipartContent "$($args[3])/odata/Processes/UiPath.Server.Configuration.OData.UploadPackage" -Method Post -Authentication Bearer -Token ($tokenstring)
 Write-Output "The package has been successfully published to Orchestrator and nexus"
--- vars/orchPublish.groovy
+++ vars/orchPublish.groovy
@@ -1,5 +1,8 @@
-def call() {
-    def psscript = libraryResource 'orchPublish.ps1'
+def call(String builddir) {
+    withCredentials([usernamePassword( credentialsId: 'orchestrator-authentication', 
+                     usernameVariable: 'user', passwordVariable: 'pwd' )])
+
+    def psscript = libraryResource 'orchPublish.ps1 VerticalApps $user $pwd https://uipath.verticalapps.com $builddir'
 
     psCall = pwsh returnStdout: true, script: psscript
     println psCall
