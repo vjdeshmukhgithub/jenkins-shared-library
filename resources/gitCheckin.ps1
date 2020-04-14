3ee16a5a8cd3d029c0574590d2bb45f8c81af02f
Justin <jlea@verticalapps.com>
2020-04-14 16:09:29 -0400

    Removing git check-in

--- resources/gitCheckin.ps1
+++
@@ -1,3 +0,0 @@
-& git.exe add "$env:WORKSPACE\project.json"
-& git.exe commit -m "Checking in version updates caused by Jenkins build job"
-& git.exe push
--- vars/gitCheckin.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-    def psscript = libraryResource 'gitCheckin.ps1'
-    psCall = powershell returnStdout: true, script: psscript
-}
